ba974cc4d3af4f3c84e4d42f867a3a55cb5748f6
Benjamin Wong <ikatadrop@gmail.com>
2021-01-05 00:40:21 -0800

    changed font

---
+++ assets/js/panels.js
@@ -0,0 +1,29 @@
+var elements = document.getElementsByClassName("page");
+
+function FocusPage(pageID) {
+   /* Unknown usage.
+   for(var i = 0; i < elements.length; i++) {
+      if(elements[i].classList.contains("main")) {
+         var currPage = i;
+      }
+   }*/
+
+   for(var i = 0; i < elements.length; i++) {
+      if(i == pageID) {
+         elements[i].classList.add("main");
+         if(elements[i].classList.contains("hidden")) {
+         elements[i].classList.remove("hidden");
+         } else if(elements[i].classList.contains("hiding")) {
+         elements[i].classList.remove("hiding");
+         }
+      } else if(elements[i].classList.contains("main")) {
+         elements[i].classList.remove("main");
+         elements[i].classList.add("hiding");
+      } else if(elements[i].classList.contains("hiding")) {
+         elements[i].classList.remove("hiding");
+         elements[i].classList.add("hidden");
+      } else if(!elements[i].classList.contains("hidden")) {
+         elements[i].classList.add("hidden");
+      }
+   }
+}
