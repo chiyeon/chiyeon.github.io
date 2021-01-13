826eb4664b1fa3c21e15e284159de3ea8e63fce4
Benjamin Wong <ikatadrop@gmail.com>
2021-01-12 20:59:10 -0800

    added new video pages

--- assets/js/panels.js
+++ assets/js/panels.js
@@ -8,16 +8,18 @@ function FocusPage(pageID) {
       }
    }*/
 
+   localStorage.setItem("page", pageID);
+
    for(var i = 0; i < elements.length; i++) {
       if(i == pageID) {
-         elements[i].classList.add("main");
+         elements[i].classList.add("introducing");
          if(elements[i].classList.contains("hidden")) {
             elements[i].classList.remove("hidden");
          } else if(elements[i].classList.contains("hiding")) {
             elements[i].classList.remove("hiding");
          }
-      } else if(elements[i].classList.contains("main")) {
-         elements[i].classList.remove("main");
+      } else if(elements[i].classList.contains("introducing") || elements[i].classList.contains("main")) {
+         elements[i].classList.remove("introducing");
          elements[i].classList.add("hiding");
       } else if(elements[i].classList.contains("hiding")) {
          elements[i].classList.remove("hiding");
@@ -28,6 +30,25 @@ function FocusPage(pageID) {
    }
 }
 
+window.addEventListener("load", () => {
+   var newPage = localStorage.getItem("page");
+
+   if(newPage == undefined)
+      return;
+
+   for(var i = 0; i < elements.length; i++) {
+      if(newPage == i) {
+         elements[i].setAttribute("class", "page main");
+         console.log("HIm");
+      } else {
+         elements[i].setAttribute("class", "page hidden");
+      }
+   }
+}, false);
+
+// dynamic video switching has been removed in favor of individual web pages per video.
+
+// === DEPRACATED ================
 function PlayVideo(title, tagline, videoID) {
    // set the title, tagline, and iframe data of video player
    document.getElementById("video-player-title").textContent = title;
@@ -41,3 +62,5 @@ function PlayVideo(title, tagline, videoID) {
 function StopVideo() {
    document.getElementById("video-player").contentWindow.postMessage('{"event":"command","func":"' + 'stopVideo' + '","args":""}', '*');
 }
+
+// === END DEPRACATED ============
