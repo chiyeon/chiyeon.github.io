33923284e5cd8f9637c243e3abd2b782ae063f6a
Benjamin Wong <ikatadrop@gmail.com>
2021-01-17 14:37:15 -0800

    justified titles for videos to center

--- assets/js/scrollfade.js
+++ assets/js/scrollfade.js
@@ -1,14 +1,3 @@
-window.onload = function() {
-   var hint = document.getElementById("info-hint");
-   var page = document.getElementById("page");
-
-   page.onscroll = function() {
-      console.log("HI");
-      var scrollTop = document.scrollTop();
-      console.log(scrollTop);
-   }
-}
-
 window.onscroll = function() {
    console.log("hmm");
 }
