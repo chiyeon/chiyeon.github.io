562408c0e90a3ed38a2d21b6374aa255eaad388c
Benjamin Wong <chi@thonkpad-pro.local>
2023-06-17 20:24:48 -0700

    woodstock v1

--- js/logic.js
+++ js/logic.js
@@ -2,6 +2,7 @@ import { Chess } from "../lib/chess.js"
 
 var game = new Chess()
 var board = null
+var board_flipped = false
 
 // 'w' or 'b'
 var player_color = "w"
@@ -44,11 +45,25 @@ const attempt_move_piece = (source, target) => {
     } catch {
         return "snapback"
     }
+    show_evaluated_position()
 
-    window.setTimeout(() => {
-        make_cheez_move(game, player_color)
+    if (game.isGameOver()) return player_win()
+
+    window.setTimeout(async () => {
+        await perform_best_move(game, player_color)
         board.position(game.fen())
+
+        show_evaluated_position()
+        if (game.isGameOver()) return computer_win()
     }, 200)
+}
+
+const player_win = () => {
+    $("#winner").text("You won!").removeClass("hidden")
+}
+
+const computer_win = () => {
+    $("#winner").text("Woodstock won!").removeClass("hidden")
 }
 
 board = Chessboard("board", {
@@ -61,25 +76,32 @@ board = Chessboard("board", {
     },
     onSnapEnd: () => {
         board.position(game.fen())
+        show_evaluated_position()
     },
     onDrop: attempt_move_piece
 })
 
-const set_board = () => {
+const set_board = async () => {
     
     game.reset()
     board.position(game.fen())
 
     if (player_color == "b") {
-        make_cheez_move(game, player_color)
+        if (!board_flipped) {
+            board.flip()
+            board_flipped = !board_flipped
+        }
+
+        await perform_random_opening_move(game)
         board.position(game.fen())
-        board.flip()
+        show_evaluated_position()
     }
 }
 
 const toggle_player_color = () => {
     if (player_color == "b") {
         board.flip()
+        board_flipped = !board_flipped
     }
 
     player_color = (player_color == "w" ? "b" : "w")
@@ -91,10 +113,30 @@ const undo_move = () => {
     game.undo()
     if (game.turn() != player_color) game.undo()
     board.position(game.fen())
+    show_evaluated_position()
+}
+
+var turn = "b"
+
+const show_evaluated_position = () => {
+    let score = evaluate_position(game, game.moves({ raw: true }))
+    $("#evaluation").text(score + "(+white, -black)")
+
+    // if (!game.isGameOver()) {
+    //     window.setTimeout(async () => {
+    //         await perform_best_move(game, turn)
+    //         turn = (turn == "w" ? "b" : "w")
+    //         board.position(game.fen())
+    //         show_evaluated_position()
+    //     }, 200)
+    // }
 }
 
 $("#undo-button").on("click", () => { undo_move() })
 $("#swap-button").on("click", () => { toggle_player_color() })
+$("#random-button").on("click", () => { if (Math.random() < 0.5) toggle_player_color() })
 $("#reset-button").on("click", () => { set_board() })
 
 set_board()
+show_evaluated_position()
+//board.flip()
